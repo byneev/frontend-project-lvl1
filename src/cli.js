d20abd456fc2892dc8e7feaf3870b883f0d58c98
Stanislav Byneev <byneev.frontend@gmail.com>
2022-04-18 14:17:00 +0300

    feat: add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,3 @@
 import readlineSync from 'readline-sync';
 
-export const getUserName = () => {
-  return readlineSync.question('May I have your name? ');
-}
+export default () => readlineSync.question('May I have your name? ');
